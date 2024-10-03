ed718ee7e8481d4447ed76ff9f40c7882c8deed8
Prachii <97833293+prachibarnwal@users.noreply.github.com>
2024-10-03 23:53:41 +0530

    Add files via upload

---
+++ 26_factors_of_num.c
@@ -0,0 +1,14 @@
+// program to print factors of a number
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+    printf("\nFactors of %d are : ",n);
+    for(int i = 1;i<=n;i++)
+    {
+        if(n%i == 0)
+            printf("%d  ",i);
+    }
+    return 0;
+}
